ef97bdd5d9e4d245cbdfc57032a12956d8fb21f3
Nyamao Gregory Kaiser$ <nyamaokaiser79@gmail.com>
2023-11-05 15:45:35 +0300

    This is a desighner website for tour and travel packages

---
+++ includes/footer.php
@@ -0,0 +1,20 @@
+
+
+
+
+<!--- /footer-top ---->
+<!---copy-right ---->
+<div class="copy-right">
+	<div class="container">
+	
+		<div class="footer-social-icons wow fadeInDown animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">
+			<ul>				
+				<li><a href="https://Twitter.com/McKaiser11" target="_blank" class="twitter"><i class="bx bxl-twitter"></i></a></li>
+
+			</ul>
+		</div>
+		<p class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;"><a href="https://nyamaogregorykaiser.github.io/Nyamao-Gregory/">Mc Greg developers</a></p>
+		
+	</div>
+	
+</div>
